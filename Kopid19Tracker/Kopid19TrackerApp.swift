b4155693c4f66d7b486a2f0c1f8f64eb157b359d
Izzan Oktiadi <zand.only@gmail.com>
2020-11-24 15:54:37 +0700

    Initial Commit

---
+++ Kopid19Tracker/Kopid19TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kopid19TrackerApp.swift
+//  Kopid19Tracker
+//
+//  Created by Izzan Oktiadi on 24/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Kopid19TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
